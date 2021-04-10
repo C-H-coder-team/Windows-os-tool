e8ef02030cd734af79f51c67f4cf05e3aef632b2
fanjun <78690857+hacker-Fanjun@users.noreply.github.com>
2021-04-10 20:01:23 +0800

    Update and rename windows tool(version 1.0 beta).cpp to windows tool(version 1.1 beta).cpp

--- windows tool(version 1.1 beta).cpp
+++ windows tool(version 1.1 beta).cpp
@@ -1,28 +1,30 @@
 #include"Display_Time.h"
 #include"KMS.h"
 #include"shutdown.h"
-using namespace std;
-//Í·ÎÄ¼ş¡¢ÃüÃû¿Õ¼äµ÷ÓÃ
+#include"Python_install.h"
+//å¤´æ–‡ä»¶ã€å‘½åç©ºé—´è°ƒç”¨
 int main()
 {
 	cout << "welcome to this os tools!" << endl
 		<< "1.activate the system by kms." << endl
 		<< "2.system power tools." << endl
 		<< "3.the time of now." << endl
-		<< "4.informaiton of the coder." << endl;
+		<< "4.install some moudles of python" << endl
+		<< "5.informaiton of the coder." << endl;
 	short i;
 	cin >> i;
 	system("cls");
 	if (i == 1)KMS();
 	if (i == 2)shutdown();
 	if (i == 3)Display_Localtime();
-	if (i == 4) 
+	if (i == 4)python_install();
+	if (i == 5) 
 	{
 		cout << "coder:fanjun" << endl;
 		Sleep(500);
-		cout << "version:v1.0 beta" << endl;
+		cout << "version:v1.1 beta" << endl;
 		Sleep(500);
-		cout << "program time: 2021/04/05" << endl;
+		cout << "program time: 2021/04/10" << endl;
 		Sleep(1000);
 	}
 	return 0;
